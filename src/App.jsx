2f332a210bddc5c2b80e7ce9a105656df268ba38
webdevratul <mahibulislamratul@gmail.com>
2023-11-06 10:43:00 +0600

    This commit includes the initial setup of React, Tailwind CSS, and React Router in the project. It also adds the necessary configuration files and dependencies.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Online Group Study</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Header</h2>
+        </div>
+    );
+};
+
+export default Header;
